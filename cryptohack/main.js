cd9a468d01cee5e0b1598eb24e53142409564391
Romer Ramos <romerramos@gmail.com>
2016-02-19 11:36:21 -0430

    Crypto finish

--- cryptohack/main.js
+++ cryptohack/main.js
@@ -1,1 +1,35 @@
-console.log("Hola mundo");
+function encode_ascii(key, first, last) {
+  first = first.charCodeAt(0);
+  last = last.charCodeAt(0);
+
+  current_key = last - key + first;
+  current_key = parseInt(current_key,10)
+  current_key = String.fromCharCode(current_key);
+  return current_key;
+}
+
+function translate(key) {
+  var letter;
+  var current_key;
+  var decrypted_key;
+  var decrypted = [];
+
+  letter = key.split('');
+
+  for(var i = 0; i < letter.length; i++) {
+    current_key = key.charCodeAt(i);
+    if(current_key >= 97 && current_key <= 122) {
+      decrypted_key = encode_ascii(current_key, 'a', 'z');
+    } else if(current_key >= 65 && current_key <= 90) {
+      decrypted_key = encode_ascii(current_key, 'A', 'Z');
+    } else {
+      decrypted_key = String.fromCharCode(current_key);
+    }
+
+    decrypted.push(decrypted_key);
+  }
+
+  return decrypted.join('');
+}
+
+console.log(translate('IlM'));
