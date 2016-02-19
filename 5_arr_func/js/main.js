7ce1c105315ae490fb75bd09ccc0e5e1248c5996
user <email@academiahack.com.ve@4geeks.co>
2016-02-19 22:52:41 +0000

    array func validado y funcional

--- 5_arr_func/js/main.js
+++ 5_arr_func/js/main.js
@@ -1,3 +1,10 @@
+// Check if a variable is a number
+function isInt(value) {
+  return !isNaN(value) && 
+         parseInt(Number(value)) == value && 
+         !isNaN(parseInt(value, 10));
+}
+
 function factoryFunc(number) {
   var arr_func = [];
   for(var i= 0; i<number; i++) {
@@ -12,10 +19,31 @@ function wrapper(number) {
   }
 }
 
-$("#capacity").keyup( function() {
-  var example_test = $("#text").val();
-  var text_to_find = $("#get").val();
-  var new_word = $(this).val();
-  var replace = example_test.replaceAll(text_to_find, new_word);
-  $("#answer").html(replace);
+var capacity_callback = function() {
+  var capacity_text = $("#capacity").val();
+  if(capacity_text.length > 0){
+    var number_of_elements = parseInt(capacity_text);
+    arr = factoryFunc(number_of_elements);
+    $('#text').val(0).attr('max',number_of_elements > 0 ? number_of_elements - 1 : 0).prop('disabled', false);
+    $('#answer').html("?");
+  }else{
+    $('#text').prop('disabled', true);  
+  }
+}
+
+var array_callback = function() {
+  var index = parseInt($("#text").val());
+  var answer = arr[index]();
+  
+  $("#answer").html(answer);
+}
+
+$('document').ready(function(){
+  var arr;
+
+  $("#capacity").change(capacity_callback);
+
+  $("#text").change(array_callback);
 });
+
+
