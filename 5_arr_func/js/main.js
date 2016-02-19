94fa1824efd422284fd9d9b920e845f89c3d207f
user <email@academiahack.com.ve@4geeks.co>
2016-02-19 22:56:14 +0000

    buenaspracticas en arrayfunc

--- 5_arr_func/js/main.js
+++ 5_arr_func/js/main.js
@@ -19,27 +19,27 @@ function wrapper(number) {
   }
 }
 
-var capacity_callback = function() {
-  var capacity_text = $("#capacity").val();
-  if(capacity_text.length > 0){
-    var number_of_elements = parseInt(capacity_text);
-    arr = factoryFunc(number_of_elements);
-    $('#text').val(0).attr('max',number_of_elements > 0 ? number_of_elements - 1 : 0).prop('disabled', false);
-    $('#answer').html("?");
-  }else{
-    $('#text').prop('disabled', true);  
-  }
-}
-
-var array_callback = function() {
-  var index = parseInt($("#text").val());
-  var answer = arr[index]();
-  
-  $("#answer").html(answer);
-}
-
 $('document').ready(function(){
   var arr;
+
+  function capacity_callback() {
+    var capacity_text = $("#capacity").val();
+    if(capacity_text.length > 0){
+      var number_of_elements = parseInt(capacity_text);
+      arr = factoryFunc(number_of_elements);
+      $('#text').val(0).attr('max',number_of_elements > 0 ? number_of_elements - 1 : 0).prop('disabled', false);
+      $('#answer').html("?");
+    }else{
+      $('#text').prop('disabled', true);  
+    }
+  }
+
+  function array_callback() {
+    var index = parseInt($("#text").val());
+    var answer = arr[index]();
+    
+    $("#answer").html(answer);
+  }
 
   $("#capacity").change(capacity_callback);
 
